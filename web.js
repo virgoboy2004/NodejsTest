3f7ad9c712bf9745647336d83275803b074919ac
Jack Zhang(MacBook) <wenjiezhang2013@gmail.com>
2014-02-11 22:49:44 -0800

    Added index.html

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var util = require("util");
+var fs = require("fs");
 var app = express();
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.redirect('./index.html');
+  res.writeHead(200, {'content-type': 'text/html'});
+        var rs = fs.createReadStream('index.html');
+        util.pump(rs, res);
 });
 
 var port = Number(process.env.PORT || 5000);
