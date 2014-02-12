0d03b60b6b614bada77bf6fdad10cbc89abae1ac
Jack Zhang(MacBook) <wenjiezhang2013@gmail.com>
2014-02-11 22:41:32 -0800

    Added index.html

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('<h1>Hello World!</h1>');
+  res.redirect('./index.html');
 });
 
 var port = Number(process.env.PORT || 5000);
